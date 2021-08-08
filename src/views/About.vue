0e1d37e7e030d896cac49807eb3435c86179d054
mogpon <aozon24@gmail.com>
2021-08-08 22:16:39 +0900

    commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p class="main">current team:{{ teamName }}</p>
+    <button @click="count">counter</button>
+    <p class="main">{{ $store.state.sum }}</p>
+    <p>Last team name clicked:{{ teamName }}</p>
+    <router-link to ="/">戻る</router-link>
   </div>
 </template>
+
+
+<script>
+export default {
+  props:['teamName'],
+  methods:{
+    count(){
+      this.$store.commit('countUP');
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .main{
+    font-weight: bold;
+    font-size:20px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>チーム一覧</h2>
+    <ul v-for="team in teams" :key="team.name">
+      <li><router-link :to ="{name:'About',params:{teamName:team.name}}">{{ team.name }}</router-link></li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ],
+    };
   }
-}
+};
 </script>
+
+<style scoped>
+div {
+  line-height: 30px;
+}
+ul{
+  padding:0;
+  text-align: center;
+  list-style: none;
+}
+</style>
